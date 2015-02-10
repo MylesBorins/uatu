4244a7d638b002287d4795252b6b018cab43645b
Myles Borins <myles@famo.us>
2015-02-10 14:02:56 -0800

    Working with single file

--- test/test-symlink.js
+++ test/test-symlink.js
@@ -7,8 +7,6 @@ var test = require('tape');
 var mkdirp = require('mkdirp');
 
 var chokidar = require('chokidar');
-// var gaze = require('gaze');
-// var fsevents = require('fsevents');
 
 var basePath = path.join(os.tmpdir(), 'uatu-' + Date.now().toString());
 var origin = path.join(basePath, 'first');
@@ -41,114 +39,15 @@ test('symlink: setup', function (t) {
   t.ok(true, 'all done!');
 });
 
-test('symlink: chokidar', function (t) {
-  t.test('* change to non symlinked file', function (tt) {
-    tt.plan(2);
-    var count = 0;
-    var watcher = chokidar.watch(indexer(origin), {
-      persistent: true
-    });
-
-    watcher.on('all', function (event) {
-      console.log(event);
-      count++;
-      tt.ok(1);
-      if (count === 2) {
-        watcher.close();
-      }
-    });
-    writeIt(origin);
+test('change to non symlinked file', function (t) {
+  t.plan(1);
+  var watcher = chokidar.watch(indexer(origin), {
+    persistent: true
   });
-  t.end();
+
+  watcher.on('change', function (event) {
+    t.ok(1, event);
+    watcher.close();
+  });
+  writeIt(origin);
 });
-
-// test('symlink: fsevents', function(t) {
-//   t.plan(1);
-//   var watcher = fsevents(indexer(origin));
-//   watcher.on('change', function (path, info) {
-//     watcher.stop();
-//     t.ok(path);
-//     console.log(path);
-//     console.log(info);
-//   });
-//   watcher.start();
-// });
-
-// test('symlink: fswatch', function (t) {
-  // var watcher = fs
-// });
-
-//   // t.test('* change to double symlinked file', function (tt) {
-//   //   tt.plan(1);
-//   //   tt.ok(true, 'beep boop');
-//   // });
-//   // t.test('* change to double symlinked file', function (tt) {
-//   //   tt.plan(1);
-//   //   tt.ok(true, 'beep boop');
-//   // });
-//   t.end();
-// });
-// test('chokidar: damn it chokidar', function (t) {
-
-  // w.close();
-
-
-  //
-  // console.log(watcher)
-  // t.end();
-// });
-
-// test('chokidar: non symlinked file', function (t) {
-//   var filePath = path.join(__dirname, 'fixturs', 'changeme.js');
-//   var w = chokidar.watch(filePath, {
-//     persistent: true
-//   });
-//   w.on('change', function (file) {
-//     console.log('boogie woogie');
-//     t.equal(file, indexer(origin));
-//     t.end();
-//   });
-//   // writeIt(origin);
-// });
-//     // tt.end();
-//     tt.plan(1);
-//     var target = indexer(origin);
-//     gaze(target, function (err, watcher) {
-//       tt.error(err);
-//       watcher.close();
-//       return;
-//     });
-//     return;
-    // hyperion.on('changed', function (filepath) {
-    //   console.log('wat?');
-    //   t.equal(filepath, target);
-    // });
-
-    // writeIt(origin);
-    // hyperion.close();
-
-    // tt.ok(1);
-    // gaze(indexer(origin), function (err, watcher) {
-//       tt.error(err, 'there should be no error from gaze when creating a watcher');
-//       tt.ok(1);
-//       // watcher.on('changed', function (filePath) {
-//         // tt.equal(filePath, indexer(origin));
-//         // tt.end();
-//       // });
-//       watcher.close();
-//       // writeIt(origin);
-//     });
-    // tt.ok(true, 'beep boop');
-  // });
-  // t.test('* change to single symlinked file', function (tt) {
-  //   tt.plan(1);
-  //   tt.ok(true, 'beep boop');
-  // });
-  // t.test('* change to double symlinked file', function (tt) {
-  //   tt.plan(1);
-  //   tt.ok(true, 'beep boop');
-  // });
-  // console.log('frustrating');
-  // return;
-  // t.end();
-// });
